0676cde4af7139718e46378192628bd80ec33453
arijitdeb1502 <arijitdeb.git@gmail.com>
2019-12-29 13:09:21 +0800

    Intergrated Open Api specification/Swagger in the app

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+
+const swaggerDocument = YAML.load(__dirname+'/endpoints/userSignup.yaml');
 const dbConnection = require('./database/connection');
 const userRouter = require('./routers/user');
-
 const app = express();
 
 // db connectivity
@@ -16,5 +19,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/v1/user', userRouter);
+
+// API Documentation
+if (process.env.NODE_ENV != 'production') {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+}
 
 module.exports = app;
